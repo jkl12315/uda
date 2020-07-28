2ca239760e70015cc8beefbeaa255ad1906632af
jkl12315 <1298552783@qq.com>
2020-07-28 10:32:50 +0800

    Update HibernateUtils.java

--- src/utils/HibernateUtils.java
+++ src/utils/HibernateUtils.java
@@ -5,9 +5,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtils {
+	//由于是静态常量配置类，所以我们设置static final
 	public static final Configuration cfg;
 	public static final SessionFactory sf;
-	
+	public int a=0;
 	static {
 		cfg=new Configuration().configure();
 		sf=cfg.buildSessionFactory();
